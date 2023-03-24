0963661ddc64f2f12b797dcf9d3e989e9744baa0
JoEpfl <126511641+JoEpfl@users.noreply.github.com>
2023-03-24 23:15:14 +0100

    Pour que j'aie les fichier graphique sur la VM

---
+++ src/graphics.cc
@@ -0,0 +1,14 @@
+/**
+ * @file graphics.cc
+ * @author J.Ropars 364156
+ * @date 2023-03-24
+ * @version 0.1
+ * @brief Pour le GUI avec GTKmm4 (implémentation)
+ */
+
+/*Note: je ne sais pas comment on est censé compiler ce fichier, avec quel makefile ?
+quelles options de compilation ??
+*/
+#include <iostream>
+#include "graphics.h"
+//#include "gtkmm.h"
---
+++ src/graphics.h
@@ -0,0 +1,14 @@
+/**
+ * @file graphics.h
+ * @author J.Ropars 364156
+ * @date 2023-03-24
+ * @version 0.1
+ * @brief Pour le GUI avec GTKmm4
+ */
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+//#include "gtkmm.h"
+
+
+
+#endif /* GRAPHICS_H */
